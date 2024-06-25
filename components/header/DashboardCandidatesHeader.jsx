8e8db92eecedadee91848b4620b670e2c607e497
MiN1One <git_min1one@mail.ru>
2024-06-26 00:01:20 +0500

    fix: user image on main

--- components/header/DashboardCandidatesHeader.jsx
+++ components/header/DashboardCandidatesHeader.jsx
@@ -122,7 +122,7 @@ const DashboardCandidatesHeader = () => {
                                             data-bs-toggle="dropdown"
                                             aria-expanded="false"
                                         >
-                                            <Image
+                                            <img
                                                 alt="avatar"
                                                 className="thumb"
                                                 src={user.avatar}
